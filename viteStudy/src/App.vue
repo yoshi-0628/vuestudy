d7cfc4803424b1a37975e00ea225079585022124
yoshi-0628 <59409129+yoshi-0628@users.noreply.github.com>
2021-02-17 23:12:49 +0900

    yosh-iijima
    vuerouter

--- viteStudy/src/App.vue
+++ viteStudy/src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div class="container">
+    <h2>Login to Feedly</h2>
+
+    <form method="post">
+      <input
+        type="email"
+        autocapitalize="off"
+        autocorrect="off"
+        name="login"
+        class="input-text input-top"
+        placeholder="Email"
+        required=""
+        value=""
+      />
+      <input
+        type="password"
+        autocapitalize="off"
+        autocorrect="off"
+        name="password"
+        class="input-text input-bottom"
+        placeholder="Password"
+        required=""
+      />
+      <input type="hidden" name="csrfToken" value="l1550s:1613570465018" />
+      <input type="submit" class="button" value="Login" />
+    </form>
+    <div class="footer">
+      <a class="lnk" href="#">Forgot your password?</a>
+      <a class="lnk" href="#">sign up</a>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+
+<style>
+</style>
