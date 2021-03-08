adccc5eccf4e054c81be17f3cde3f07585831314
yoshi-0628 <59409129+yoshi-0628@users.noreply.github.com>
2021-03-09 00:13:10 +0900

    yosh
    eslint prettier

--- first-firebase/src/components/HelloWorld.vue
+++ first-firebase/src/components/HelloWorld.vue
@@ -3,21 +3,7 @@
     <h1>Hello {{ name }}!!</h1>
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <button @click="getFireStore">dad firestore</button>
     <button @click="signOut">Sign out</button>
   </div>
 </template>
@@ -30,14 +16,31 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      name: firebase.auth().currentUser.email
+      name: firebase.auth().currentUser.email,
+      db: firebase.firestore().collection('users'),
+      userName: ''
     }
+  },
+  computed: {
   },
   methods: {
     signOut: function () {
       firebase.auth().signOut().then(() => {
         this.$router.push('/signin')
       })
+    },
+    getFireStore: function () {
+      const query = this.db.where('mail', '==', 'yoshihisa.drum@gmail.com')
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            return doc
+          })
+        })
+        .catch((error) => {
+          console.log('Error getting documents: ', error)
+        })
+      console.log(query)
     }
   }
 }
